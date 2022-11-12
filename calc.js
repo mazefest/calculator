2cf57cc6ec062379632ece6612a3af85d2c03801
Mazefest <colbymehmen@gmail.com>
2022-11-12 15:03:32 -0600

    Added computation logic

--- calc.js
+++ calc.js
@@ -14,6 +14,22 @@ reader.question("What would you like to calculate?", function(input){
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
 
+	let result = 0
+
+	if (mathSymbol === "+") {
+		result = num1 + num2
+	} else if (mathSymbol === "-") {
+		result = num1 - num2
+	} else if (mathSymbol === "/") {
+		result = num1 / num2
+	} else if (mathSymbol == "*") {
+		result = num1 * num2
+	} else if (mathSymbol == "√") {
+		result = Math.sqrt(num1)
+	}
+ 
+	console.log("Result: " + result)
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
